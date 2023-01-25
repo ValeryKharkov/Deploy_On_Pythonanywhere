8b7f2bb588cde6da284a9dc505afc6ca2346b4f0
ValeryKharkov <kharkov.valery@gmail.com>
2023-01-25 14:48:38 +0300

    Проект работает на локальном хосте

--- Deploy_On_Pythonanywhere/settings.py
+++ Deploy_On_Pythonanywhere/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -25,8 +24,8 @@ SECRET_KEY = 'django-insecure-of_n6z@g^yz*k9_3182c(8-mh$(st997m_*(y2nk9yk$6oj*37
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+# ALLOWED_HOSTS = []  # исходная строка
+ALLOWED_HOSTS = ['*']  # Харьков: изменение согласно инструкции
 
 # Application definition
 
@@ -69,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Deploy_On_Pythonanywhere.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -79,7 +77,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -99,7 +96,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -110,7 +106,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
---
+++ readme.md
@@ -0,0 +1,4 @@
+Описание: эксперименты с подключением django-проекта к облачной платформе
+Цель: развернуть django-проект на PythonAnywhere
+Оф.документация: https://help.pythonanywhere.com/pages/DeployExistingDjangoProject/
+Порядок действий: https://progler.ru/blog/kak-razvernut-proekt-django-na-pythonanywhere
